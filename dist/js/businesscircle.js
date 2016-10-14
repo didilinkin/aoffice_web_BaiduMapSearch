a0dfaa407b9dbc14d8eb533f8ce38d56728cd720
yanxiaodi <929213769@qq.com>
2016-10-14 14:02:16 +0800

    合并新检索列表，并添加两个地图按钮;2级与3级模型数据的示例

---
+++ dist/js/businesscircle.js
@@ -0,0 +1,22 @@
+// 商圈数据
+/*
+    每个行政区做2个商圈数据,共五个行政区
+*/
+var businessCircle = [
+    // 注释格式：市南区-商圈-XXX
+    {
+        code: "",                   // 编码
+        name: "",                   // 商圈名称
+        longitude: 36.081759,       // 商圈经度
+        latitude: 120.389053,       // 商圈纬度
+        resourceAmount: 100         // 资源数量
+    },
+    // 注释格式：市南区-商圈-XXX
+    {
+        code: "",                   // 编码
+        name: "",                   // 商圈名称
+        longitude: 36.081759,       // 商圈经度
+        latitude: 120.389053,       // 商圈纬度
+        resourceAmount: 100         // 资源数量
+    }
+]
---
+++ dist/js/searchmodel.js
@@ -0,0 +1,40 @@
+// 实体楼盘
+/*
+    每个商圈做2个实体楼盘数据
+*/
+var SearchModel = [
+    // 注释格式：写字楼-XXX
+    {
+        cityCode: "",                               // 编码
+        regionCode: "",                             // 楼盘所属行政区
+        businessCircleCode: 36.081759,              // 楼盘所属商圈
+        buildingCode: 120.389053,                   // 楼盘编码
+        name: ,                                     // 楼盘名称
+        type: ,                                     // 资源类型
+        decoration: ,                               // 装修（豪装.精装.简装.毛坯）
+        areaMin: ,                                  // 资源面积最小值
+        areaMax: ,                                  // 资源面积最大值
+        priceDayMin: ,                              // 价格按天最小值
+        priceDayMax: ,                              // 价格按天最大值
+        longitude: 36.081759,                       // 楼盘经度
+        latitude: 120.389053,                       // 楼盘纬度
+        resourceAmount: 100                         // 资源数量
+    },
+    // 注释格式：写字楼-XXX
+    {
+        cityCode: "",                               // 编码
+        regionCode: "",                             // 楼盘所属行政区
+        businessCircleCode: ,                       // 楼盘所属商圈
+        buildingCode: ,                             // 楼盘编码
+        name: ,                                     // 楼盘名称
+        type: ,                                     // 资源类型
+        decoration: ,                               // 装修（豪装.精装.简装.毛坯）
+        areaMin: ,                                  // 资源面积最小值
+        areaMax: ,                                  // 资源面积最大值
+        priceDayMin: ,                              // 价格按天最小值
+        priceDayMax: ,                              // 价格按天最大值
+        longitude: ,                                // 楼盘经度
+        latitude: ,                                 // 楼盘纬度
+        resourceAmount: 100                         // 资源数量
+    }
+]
