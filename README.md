7f5cbca72bf471afedfa4b3e58085bd12c1dd311
yanxiaodi <929213769@qq.com>
2016-10-17 11:40:46 +0800

    修改:覆盖物文字居中,调整声明位置;

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # A+Office地图找房页－百度地图项目
 > aoffice_web_BaiduMapSearch
+修改文字居中
+确认层级是否跟点点租相同
+改变声明位置
--- dist/js/call.js
+++ dist/js/call.js
@@ -13,15 +13,59 @@ map.enableScrollWheelZoom(true);
 map.addEventListener("zoomend", function(){
     var zoomLevel = this.getZoom(),     //　当前地图级别
         centerPoint = this.getCenter(); //  当前中心点坐标
-    if (zoomLevel>=14&&zoomLevel<15){
+    if ( zoomLevel>=14&&zoomLevel<15 ){
 		map.clearOverlays();    // 清理地图上面所有点
 		addRangeOverlay(businessCirclePoint,16);
 		// console.log("输出2级地图内容:商圈");		// 商圈自定义覆盖物
-    }else if (zoomLevel>=15){
+    }else if ( zoomLevel>=15 ){
         // console.log("输出3级地图内容:详细覆盖");
-    }else{
+    }else {
 		map.clearOverlays();    // 清理地图上面所有点
 		addRangeOverlay(RegionPoint,14);		// 输出行政区自定义覆盖物
 		// console.log("输出1级地图内容:行政区");
     }
 });
+// 行政区＋商圈范围覆盖物——１.2级通用
+function rangeOverlay(point,text,code,url,zoom){
+    this._point = point;
+    this._text = text;
+    this._code = code;
+    this._url = url;
+    this._zoom = zoom;
+}
+rangeOverlay.prototype = new BMap.Overlay();
+rangeOverlay.prototype.initialize = function(map){
+    this._map = map;
+    var div = this._div = document.createElement("div");
+    div.setAttribute("id",this._code);
+    div.setAttribute("class","range-overlay");
+    div.style.zIndex = BMap.Overlay.getZIndex(this._point.lat);
+    // 保存code
+    var code = this._code,   //　区域代码
+        url = this._url,
+        point = this._point,
+        zoom = this._zoom;
+    div.onclick = function businessCirclePoint(){
+        // Ajax上传code，并改变中心点
+        map.setZoom(zoom);      // 根据坐标点进行跳转,改变层级
+        // console.log("跳转链接" + url);
+    }
+    var span = this._span = document.createElement("span");
+    div.appendChild(span);
+    div.getElementsByTagName("span")[0].innerHTML =  this._text;
+    div.onmouseover = function(){
+        this.style.zIndex = "9";
+    }
+    div.onmouseout = function(){
+        this.style.zIndex = "1";
+    }
+    map.getPanes().labelPane.appendChild(div);
+    return div;
+}
+rangeOverlay.prototype.draw = function(){
+    var map = this._map;
+    var pixel = map.pointToOverlayPixel(this._point);
+    // this._div.style.left = pixel.x - parseInt(this._arrow.style.left) + "px";
+    this._div.style.left = pixel.x - 30 + "px";
+    this._div.style.top  = pixel.y - 30 + "px";
+}
