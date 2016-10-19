f0d2652873661a59ea0cb0c82e7b30abcf609ade
yanxiaodi <929213769@qq.com>
2016-10-19 17:12:57 +0800

    完成3级覆盖物(信息窗口用百度官方方法实现);在3级覆盖物添加父节点,尝试将信息窗口添加到3级覆盖物的兄弟节点(备份)

--- dist/js/call.js
+++ dist/js/call.js
@@ -22,7 +22,7 @@ map.addEventListener("zoomend", function(){
 		// 	console.log("输出3级地图内容:详细覆盖");
 		// }
 		addBuilding(BuildingModel,17);
-		console.log("输出3级地图内容:详细覆盖");
+		// console.log("输出3级地图内容:详细覆盖");
 	}else if (zoomLevel >= 14){
 		addRangeOverlay(businessCirclePoint,16);
 		// console.log("输出2级地图内容:商圈");                // 商圈自定义覆盖物
@@ -61,12 +61,8 @@ rangeOverlay.prototype.initialize = function(map){
     var span = this._span = document.createElement("span");
     div.appendChild(span);
     div.getElementsByTagName("span")[0].innerHTML =  this._text;
-    div.onmouseover = function(){
-        this.style.zIndex = "9";
-    }
-    div.onmouseout = function(){
-        this.style.zIndex = "1";
-    }
+    div.onmouseover = function(){ this.style.zIndex = "9"; }
+    div.onmouseout = function(){ this.style.zIndex = "1"; }
     map.getPanes().labelPane.appendChild(div);
     return div;
 }
@@ -77,7 +73,7 @@ rangeOverlay.prototype.draw = function(){
     this._div.style.left = pixel.x - 30 + "px";
     this._div.style.top  = pixel.y - 30 + "px";
 }
-// 建筑物具体覆盖物——3级用(NO.　是　序号)
+// 建筑物具体覆盖物——3级用(NO.是序号)
 function buildingOverlay(point,text,mouseoverTxt,code,NO,zoom){
 	this._point = point;
 	this._text = text;
@@ -88,27 +84,46 @@ function buildingOverlay(point,text,mouseoverTxt,code,NO,zoom){
 }
 buildingOverlay.prototype = new BMap.Overlay();
 buildingOverlay.prototype.initialize = function(map){
+
+
+
+
+
+
+
+
     this._map = map;
-    var div = this._div = document.createElement("div");
+    var div = this._div = document.createElement("div"); // 父级元素
+        // divChildOverlay =
     div.setAttribute("class","building-overlay");
+    var num = this._NO;
     div.style.zIndex = BMap.Overlay.getZIndex(this._point.lat);
-    div.setAttribute("onclick","buildingOverlayClick(" +  this._NO　+ ")");
+    div.onclick = function () {
+        var buildingOverlayObj = BuildingModel[num];
+        // 信息窗口实例　三级建筑物覆盖物点击弹出的DIV     <div class="pointdetail clearfix">
+        var buildingContent =
+            "<h2>" + buildingOverlayObj.name + " </h2>" +
+            "<p><a href=\"#\" target=\"_blank\"><img id=\"imgDemo\" src=\"" + buildingOverlayObj.picUrl + "\" width=\"420\" height=\"235\" alt=\"\"></a></p>" +
+            "<ul><li><a href=\"#\"><span>" + buildingOverlayObj.areaMin + "m2</span><span class=\"w110\">¥ " + buildingOverlayObj.dayBeginning + "元/天</span><span>" + buildingOverlayObj.decoration + "</span><img src=\"" + buildingOverlayObj.childPic + "\" width=\"56\" height=\"35\" alt=\"\"></a></li><li class=\"more\"><a href=\"#\">查看更多（" + buildingOverlayObj.areaMin + "~" + buildingOverlayObj.areaMax + "㎡）</a></li></ul>" +
+            "</div>";
+        var infoWindow = new BMap.InfoWindow(buildingContent);  // 创建信息窗口对象
+        var point = new BMap.Point(buildingOverlayObj.latitude,buildingOverlayObj.longitude + 0.001);
+        // this.openInfoWindow(infoWindow);  无法实现在覆盖物对象上面弹出,只能靠坐标弹出
+        map.openInfoWindow(infoWindow,point); //开启信息窗口
+    }
     // // 保存code
     // var code = this.code
-    // div.onclick = function(){
-    //     console.log(code);　　// 成功打印code
-    // }
     div.style.fontSize = "12px"
     var span = this._span = document.createElement("span");
     div.appendChild(span);
     span.appendChild(document.createTextNode(this._text));
     var that = this;
-    // var arrow = this._arrow = document.createElement("div");
-    //     arrow.setAttribute("class","arrow");
-    //     arrow.style.position = "absolute";
-    //     arrow.style.top = "22px";
-    //     arrow.style.left = "10px";
-    // div.appendChild(arrow);
+    var arrow = this._arrow = document.createElement("div");
+        arrow.setAttribute("class","arrow");
+        arrow.style.position = "absolute";
+        arrow.style.top = "22px";
+        arrow.style.left = "10px";
+    div.appendChild(arrow);
     div.onmouseover = function(){
         this.getElementsByTagName("span")[0].innerHTML = that._mouseoverTxt;
     }
@@ -117,6 +132,21 @@ buildingOverlay.prototype.initialize = function(map){
     }
     map.getPanes().labelPane.appendChild(div);
     return div;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 buildingOverlay.prototype.draw = function(){
     var map = this._map;
